877684fa6267902c96c13df82c558f15f1f7bed0
linerafaela <128156711+linerafaela@users.noreply.github.com>
2024-10-31 08:16:07 -0300

    .

---
+++ main.java
@@ -0,0 +1,12 @@
+class Helloorld {
+  public static void main (String[] args) {
+    System.out.println("Esse e o Screen Match");
+    System.out.println("Filme: Coraline e o mundo secreto");
+    
+    int ano = 2009;
+    boolean incluiNoplano = true;
+    double notaFilme = 10;
+    
+    System.out.println("Ano de lançamento " + ano);
+  }
+}
