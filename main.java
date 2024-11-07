58ea9da6f21ba34a78f01fb81293853ab6edff16
linerafaela <128156711+linerafaela@users.noreply.github.com>
2024-11-07 08:09:49 -0300

    .

--- main.java
+++ main.java
@@ -8,5 +8,15 @@ class Helloorld {
     double notaFilme = 10;
     
     System.out.println("Ano de lançamento " + ano);
+    double media = (8.5 + 9.0 + 9.5)/3;
+   System.out.println("A media das notas deste filme e:" + media);
+
+   String sinopse;
+   sinopse = "Enquanto explora sua nova casa à noite, a pequena Coraline descobre uma porta secreta que contém um mundo parecido com o dela, porém melhor em muitas maneiras. Todos têm botões no lugar dos olhos, os pais são carinhosos e os sonhos de Coraline viram realidade por lá. Ela se encanta com essa descoberta, mas logo percebe que segredos estranhos estão em ação: uma outra mãe e o resto de sua família tentam mantê-la eternamente nesse mundo paralelo.";
+   System.out.println(sinopse);
+   
+   // Atribuindo estrelas
+   int classificação = (int) media/2;
+   System.out.println
   }
 }
